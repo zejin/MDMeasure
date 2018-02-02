d6583483934f6401b03e871cf4433f81d038deec
Ze Jin <btluck1989@gmail.com>
2018-02-02 15:23:33 -0500

    cpp -

--- R/MDMeasure-package.R
+++ R/MDMeasure-package.R
@@ -2,14 +2,14 @@
 #'
 #' The MDMeasure package provides measures of mutual dependence and tests of mutual independence. 
 #' 
-#' @section Measuring mutual dependence
+#' @section Measuring mutual dependence:
 #' The mutual dependence measures:
 #' - complete measure based on complete V-statistics
 #' - simplified complete measure based on incomplete V-statistics
 #' - asymmetric measure based on distance covariance
 #' - symmectric measure based on distance covariance 
 #'
-#' @section Initializing local optimization methods
+#' @section Initializing local optimization methods:
 #' The permutation tests based on the mutual dependence measures.
 #'
 #' @name MDMeasure-package
