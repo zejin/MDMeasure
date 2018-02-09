a631728fac336fe7b1fa8124d9d2a8335146ea9d
Ze Jin <btluck1989@gmail.com>
2018-02-08 19:42:39 -0500

    add match

---
+++ dev/match_result.R
@@ -0,0 +1,37 @@
+#
+set.seed(12345)
+X <- matrix(rnorm(10 * 3), 10, 3)
+
+#
+mdm(X, type = "asym_dcov")
+mdm(X, type = "sym_dcov")
+mdm(X, type = "comp")
+mdm(X, type = "comp_simp")
+mdm(X, type = "asym_comp")
+mdm(X, type = "asym_comp_simp")
+mdm(X, type = "sym_comp")
+mdm(X, type = "sym_comp_simp")
+
+# cd mmi/simulation/real_data/ff_5_year
+source('dCov.R')
+set.seed(12345)
+X <- matrix(rnorm(10 * 3), 10, 3)
+num_obs <- nrow(X)
+dim_comp <- c(1, 1, 1)
+num_dim <- sum(dim_comp)
+num_comp <- length(dim_comp)
+index_comp <- cumsum(c(1, dim_comp)) # start index and end index
+X <- c(t(X))
+D <- rep(0, num_comp * num_obs * num_obs)
+
+#
+dCov_asymmetric(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+dCov_symmetric(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_complete(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_complete_simple(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_asymmetric(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_asymmetric_simple(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_symmetric(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+est_symmetric_simple(X, D, 0, num_obs, num_dim, num_comp, index_comp - 1)$Q
+
+
